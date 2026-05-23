afa091cf3f74e55919d781f3c0a91bab4fda5593
ancoratech422517 <ancoratech422517@gmail.com>
2026-05-23 16:25:35 +0100

    Update app.py

--- BackEnd-main/backend/app.py
+++ BackEnd-main/backend/app.py
@@ -106,7 +106,7 @@ CORS(app, resources={r"/*": {
         "http://192.168.11.1",
         "http://192.168.11.1:5173",
         "http://10.140.176.115:5173",
-        "https://loja-penelaka.netlify.app"
+        "https://ancora-ecommerce.vercel.app"
     ],
     "supports_credentials": True,
     "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
@@ -118,7 +118,7 @@ CORS(app, resources={r"/*": {
 def after_request(response):
     origin = response.headers.get('Access-Control-Allow-Origin')
     if not origin:
-        response.headers.add('Access-Control-Allow-Origin', 'https://loja-penelaka.netlify.app')
+        response.headers.add('Access-Control-Allow-Origin', 'https://ancora-ecommerce.vercel.app')
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
     response.headers.add('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS')
     response.headers.add('Access-Control-Allow-Credentials', 'true')
